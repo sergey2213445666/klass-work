348704109fe412e18d7e15281cfcbe4bf93f551a
sergey2213445666 <115737537+sergey2213445666@users.noreply.github.com>
2024-01-23 15:59:53 +0300

    классная работа 23.01.24

---
+++ main (1).py
@@ -0,0 +1,24 @@
+# print(text[0])
+# print(text[0:5]) 
+# print(text[4:10])
+# print(text[0:14])
+# print(text[7:])
+# print(text[:5])
+# print(text[:])
+# print(text[-1])
+# print(text[-1:-14])
+# print(text[::2])
+# print(text[::2])
+# print(text[::-1])
+# print(text + "nice to code you")
+# print(text[-1] * 10)
+
+my_list =[True, 768, 3.14, 'text', 70.2]
+second_list =[123, 'text']
+
+print(my_list)
+print(my_list[0])
+print(my_list[1:3])
+print(my_list[2:])
+print(second_list *2)
+print(my_list + second_list)
