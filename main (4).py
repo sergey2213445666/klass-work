30838aedcda5c473c5314f35dd51db886316ac6a
sergey2213445666 <115737537+sergey2213445666@users.noreply.github.com>
2024-01-26 11:54:33 +0300

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,84 @@
+# задача1
+
+x = int(input("Введите число:"))
+y = 7 * x + 5
+print("ответ: y =" ,y)
+
+#задача2
+
+a = int(input("ВВЕДИТЕ ЧИСЛО:"))
+b = a * 4
+print("ответ: периметр =" ,b)
+
+# задача3
+
+num1 = int(input("введите число:"))
+num2 = 2 * 3.14 * r 
+print("ответ: num2=" ,num2)
+
+# задача4
+
+z = int(input("введите число:"))
+l = z **2 // 4 * 3.14
+print("ответ: l=" ,l)
+
+#задача5 
+
+g = int(input("введите число:"))
+h = int(input("введите число:"))
+k = (g**2 + h**2)**0.5 
+print("ответ: k=" ,k)
+
+# задача6
+
+rew1 = int(input("введите число:"))
+rew2 = int(input("введите число:"))
+rew3 = (rew1**2 + rew2**2)**0.5
+rew4 = rew1 + rew2 + rew3
+print("ответ: rew3=" ,rew3)
+print("ответ: rew4=" ,rew4)
+
+# задача7
+
+length = int(input("введите длину прямоугольника:"))
+width = int(input("введите ширину прямоугольника:"))
+per = 2 * (length + width)
+diag = (length**2 + width**2)**0.5
+print("ответ: per=" ,per)
+print("ответ: diag=" ,diag)
+
+#задача8
+
+outer = int(input("введите радиус внешней окружности:"))
+inner = int(input("введите радиус внутренней окружности:"))
+outer_area = 3.14159 * (outer**2)
+inner_area = 3.14159 * (inner**2)
+ring = outer_area - inner_area
+print("outer_area=" ,outer_area)
+print("inner_area=" ,inner_area)
+print("ring=" ,ring)
+
+задача9
+
+edge = int(input("введите длину ребра куба: "))
+sur = 6 * (edge**2)
+vol = edge**3
+print("sur=" ,sur)
+print("vol=" ,vol)
+
+# задача10
+
+bas1 = int(input("введите длину первого основания:"))
+bas2 = int(input("введите длину второго основания:"))
+hes = int(input("введите высоту трапеции"))
+side = hes * 2 +((bas1 - bas2) // 2) * 2 
+pir = bas1 + bas2 + 2 * side
+print("ответ: pir=" ,pir)
+
+
+
+
+
+
+
+
