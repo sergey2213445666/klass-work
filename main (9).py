8eaa0418170e71b5f5149f67ce9b3b29cd1a948a
sergey2213445666 <115737537+sergey2213445666@users.noreply.github.com>
2024-02-02 11:04:34 +0300

    02.02.24 практическое задания

---
+++ main (9).py
@@ -0,0 +1,11 @@
+tseloe = 10
+drobnoe = 8,4
+stroka = "No"
+da = "yes"
+lodka = "_"
+big_tseloe = tseloe * 3,5
+drobnoe = drobnoe - 1
+print(tseloe // drobnoe)
+print(big_tseloe // drobnoe)
+stroka = stroka + stroka  + lodka + da * 3
+print(big_tseloe, drobnoe, stroka)
