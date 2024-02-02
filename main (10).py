bcac95009fa09e48e9d4af5f97e0da1e5e9e5041
sergey2213445666 <115737537+sergey2213445666@users.noreply.github.com>
2024-02-02 11:47:06 +0300

    02.02.24 практическая работа; задание 2

---
+++ main (10).py
@@ -0,0 +1,25 @@
+a = 5 
+b = 10
+num1 = a < b and a > 10
+print(num1)
+num2 = a >= b and a == b
+print(num2)
+num3 = a < b and a ** 2 == 25
+print(num3)
+num4 = b / 2 == 5 and b == 10
+print(num4)
+
+num5 = a * 2 == 10 or b - 5 == 5 
+print(num5)
+num6 = a + 5 == b or a == b 
+print(num6)
+num7 = a == b or a > b
+print(num7)
+num8 = 10 * 5 == b or 5 >= b
+print(num8)
+
+#3
+str1 = "a"
+str2 = "b"
+n = str1 > "a" and str2 < "b"
+print(n)
