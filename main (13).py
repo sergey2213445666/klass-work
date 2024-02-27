1a62c0835e54addbd215f31008df262358c39341
sergey2213445666 <115737537+sergey2213445666@users.noreply.github.com>
2024-02-27 15:11:39 +0300

    27.02.24

---
+++ main (13).py
@@ -0,0 +1,14 @@
+# fib1 = 0
+# fib2 = 1
+# print(fib1)
+# n = 10
+# i = 1 
+# while i < n:
+#     fib_sum = fib1 + fib2
+#     print (fib_sum)
+#     fib1 = fib2
+#     fib2 = fib_sum
+#     i = i + 1
+
+
+
